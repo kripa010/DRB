812e2f9578370581c17dca737e68604c697e076c
KP <KP@DESKTOP-781QH81>
2022-12-04 13:17:40 +0530

    3RD COMMIT

--- src/test/java/com/drb/script/DrbTest.java
+++ src/test/java/com/drb/script/DrbTest.java
@@ -1,7 +1,5 @@
 package com.drb.script;
-import org.testng.AssertJUnit;
 import org.testng.annotations.Test;
-
 import com.drb.pages.DrbCreateProfile;
 import com.drb.pages.DrbDeleteProfile;
 import com.drb.pages.DrbDownload;
@@ -10,33 +8,21 @@ import com.drb.pages.DrbLogin;
 import com.drb.pages.DrbLogout;
 import com.drb.pages.DrbSignUp;
 import com.drb.utilities.ExcelUtilities;
-
 import java.io.IOException;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+
+
 public class DrbTest extends DrbBase{
-	DrbLogin dlin;
+	 DrbLogin dlin;
 	 DrbSignUp dsignup;
 	 DrbCreateProfile dcrtprf;
 	 DrbEditProfile dedtprf;
-	 DrbLogout dlout;
-	 DrbDownload drbdwnld;
-//  public void loginValidate(String userName,String pass)
-//  {
-//	  dlin=new DrbLogin(driver);
-//	  dlin.clickSignUp();
-//	  dlin.setUserName(userName);
-//	  dlin.setPassword(pass);
-//	  dlin.clickRememberMe();
-//	  dlin.loginClick();
-//	  String title="http://167.71.226.96/login";
-//	  String actualTitle=driver.getCurrentUrl();
-//	  AssertJUnit.assertEquals(actualTitle, title);
-//  }
+
   @Test(priority=2)
-  public void validlogin() throws IOException, InterruptedException
+//VALID LOGIN
+  public void validlogin() throws IOException, InterruptedException     
   {   
 	  String userName=ExcelUtilities.getcelldata(0, 0, 1);
 	  String pass=ExcelUtilities.getcelldata(0, 1, 1);
@@ -50,11 +36,12 @@ public class DrbTest extends DrbBase{
 	  String title="http://167.71.226.96/";
 	  String actualTitle=driver.getCurrentUrl();
 	  System.out.println(actualTitle);
-	  Assert.assertEquals(actualTitle, title);
-	  
+	  Assert.assertEquals(actualTitle, title);	  
   }
+  
   @Test
-  public void inValidlogin1() throws IOException, InterruptedException
+  //INVALID LOGIN
+  public void inValidlogin1() throws IOException, InterruptedException      
   {
 	  String userName=ExcelUtilities.getcelldata(0, 0, 2);
 	  String pass=ExcelUtilities.getcelldata(0, 1, 2);
@@ -68,14 +55,10 @@ public class DrbTest extends DrbBase{
 	  boolean actual=loginButton.isSelected();
 	  System.out.println(actual);
 	  Assert.assertFalse(actual);
-//	  Thread.sleep(1000);
-//	  String title="http://167.71.226.96/";
-//	  String actualTitle=driver.getCurrentUrl();
-//	  System.out.println(actualTitle);
-//	  AssertJUnit.assertEquals(actualTitle, title);
   }
   
   @Test
+  //VALID SIGNUP 
   public  void validSignUp() throws IOException, InterruptedException
   {	  String username=ExcelUtilities.getcelldata(1, 0, 1);
   	  String phone=ExcelUtilities.getcelldata(1, 1, 1);
@@ -83,14 +66,12 @@ public class DrbTest extends DrbBase{
   	  String pass=ExcelUtilities.getcelldata(1, 3, 1);
   	  String cpass=ExcelUtilities.getcelldata(1, 4, 1);
 	  dsignup=new DrbSignUp(driver);
-	 // dsignup.clickSignUp();
-	 // dsignup.clickSignupLink();
 	  driver.navigate().to("http://167.71.226.96/signup");
 	  dsignup.setUserName(username);
 	  dsignup.setPhoneNumber(phone);
-	  dsignup.setMailId(ExcelUtilities.getcelldata(1, 2, 1));
-	  dsignup.setPassword(ExcelUtilities.getcelldata(1, 3, 1));
-	  dsignup.setConfirmPassword(ExcelUtilities.getcelldata(1, 4, 1));
+	  dsignup.setMailId(email);
+	  dsignup.setPassword(pass);
+	  dsignup.setConfirmPassword(cpass);
 	  dsignup.clickRegister();
 	  String expected="New user added";
 	  String actual=driver.switchTo().alert().getText();
@@ -98,7 +79,34 @@ public class DrbTest extends DrbBase{
 	  driver.switchTo().alert().accept();
 	 
   }
+  
+  @Test
+  //INVALID SIGNUP
+  public  void invalidSignUp() throws IOException, InterruptedException
+  {	  String username=ExcelUtilities.getcelldata(1, 0, 2);
+  	  String phone=ExcelUtilities.getcelldata(1, 1, 2);
+  	  String email=ExcelUtilities.getcelldata(1, 2, 2);
+  	  String pass=ExcelUtilities.getcelldata(1, 3, 2);
+  	  String cpass=ExcelUtilities.getcelldata(1, 4, 2);
+  	  dsignup=new DrbSignUp(driver);
+  	  driver.navigate().to("http://167.71.226.96/signup");
+  	  dsignup.setUserName(username);
+  	  dsignup.setPhoneNumber(phone);
+  	  dsignup.setMailId(email);
+  	  dsignup.setPassword(pass);
+  	  dsignup.setConfirmPassword(cpass);
+  	  dsignup.clickRegister();
+  	  
+  	  WebElement registerButton=driver.findElement(By.cssSelector("body > app-root > app-signup > div > div > form > button > span"));
+  		
+  	  boolean actual=registerButton.isSelected();
+  	  System.out.println(actual);
+  	  Assert.assertFalse(actual);
+  	  Thread.sleep(1000);  
+  }
+  
   @Test(priority=1)
+  //INVALID SIGNUP WITH EXISTING USERNAME
   public  void inValidSignUpWithExisting() throws IOException, InterruptedException
   {	  validSignUp();
   	  String username=ExcelUtilities.getcelldata(1, 0, 1);
@@ -112,9 +120,9 @@ public class DrbTest extends DrbBase{
 	  driver.navigate().to("http://167.71.226.96/signup");
 	  dsignup.setUserName(username);
 	  dsignup.setPhoneNumber(phone);
-	  dsignup.setMailId(ExcelUtilities.getcelldata(1, 2, 1));
-	  dsignup.setPassword(ExcelUtilities.getcelldata(1, 3, 1));
-	  dsignup.setConfirmPassword(ExcelUtilities.getcelldata(1, 4, 1));
+	  dsignup.setMailId(email);
+	  dsignup.setPassword(pass);
+	  dsignup.setConfirmPassword(cpass);
 	  dsignup.clickRegister();
 	  String expected="User already Exists";
 	  String actual=driver.switchTo().alert().getText();
@@ -122,7 +130,9 @@ public class DrbTest extends DrbBase{
 	  driver.switchTo().alert().accept();
 	 
   }
+  
   @Test(priority=3)
+  //VALID CREATE PROFILE WITH MANDATORY FIELDS
   public void validCreateProfile() throws IOException, InterruptedException
   {
 	  
@@ -139,8 +149,11 @@ public class DrbTest extends DrbBase{
    	  String lang=ExcelUtilities.getcelldata(2, 11, 1);
    	  String describe=ExcelUtilities.getcelldata(2, 12, 1);
    	  String img=System.getProperty("user.dir")+"\\"+ExcelUtilities.getcelldata(2, 13, 1);
+   	  
    	  dcrtprf=new DrbCreateProfile(driver);
+   	  
    	  validlogin();
+   	  
    	  dcrtprf.clickMyProfile();
    	  dcrtprf.clickCrtProfile();
    	  dcrtprf.setName(name);
@@ -152,7 +165,6 @@ public class DrbTest extends DrbBase{
    	  dcrtprf.setPercent(percentage);
    	  dcrtprf.setSchool(school);
    	  dcrtprf.setYear(year);
-   	  
    	  dcrtprf.setSkills(skills);
    	  dcrtprf.setLanguage(lang);
    	  dcrtprf.setDescribe(describe);
@@ -161,43 +173,73 @@ public class DrbTest extends DrbBase{
    	  dcrtprf.clickCreateProfile();
   
 }
+  
   @Test(priority=5)
+  //LOGOUT 
   public void logOut() throws IOException, InterruptedException
   {
 	  validlogin();
 	  DrbLogout dlout=new DrbLogout(driver);
 	  Thread.sleep(1000);
 	  dlout.clickLogout();
-	  
-  }
+	   }
+  
   @Test(priority=7)
+ //DELETE PROFILE
   public void deleteProfile() throws IOException, InterruptedException
   {
 	  validlogin();
-//	  validSignUp();
-//	  validCreateProfile();
 	  DrbDeleteProfile drbdlt=new DrbDeleteProfile(driver);
-	//  Thread.sleep(1000);
 	  drbdlt.deleteProfile();
 	 
 	  
   }
-  
+
   @Test(priority=6)
+ //DOWNLOAD TEMPLATE 1
   public void downloadTemplate1() throws IOException, InterruptedException
   {
 	  validlogin();
 	  DrbDownload  drbdwnld =new DrbDownload(driver);
 	  drbdwnld.clickTemplateBtn();
-	  drbdwnld.clickTemplate();
-	  drbdwnld.clickDownloadResume();
-	
+	  Thread.sleep(500);
+	  drbdwnld.clickTemplate("#web-development > div > div > div:nth-child(2) > div:nth-child(1) > a > figure > figcaption");
+	  Thread.sleep(1000);
+	  drbdwnld.clickDownloadResume("body > app-root > app-threetemp > section > button.btn.btn.btn-dark.m-1");
   }
   
-    @Test(priority=4)
-    public void validEditProfile() throws IOException, InterruptedException
+  
+  @Test
+  //DOWNLOAD TEMPLATE 2
+  public void downloadTemplate2() throws IOException, InterruptedException
   {
-	  
+	  validlogin();
+	  DrbDownload  drbdwnld =new DrbDownload(driver);
+	  drbdwnld.clickTemplateBtn();
+	  Thread.sleep(500);
+	  drbdwnld.clickTemplate("#web-development > div > div > div:nth-child(2) > div.cc-porfolio-image.img-raised.aos-init.aos-animate > a > figure > figcaption");
+	  Thread.sleep(1000);
+	  drbdwnld.clickDownloadResume("body > app-root > app-threetemp > section > button.btn.btn.btn-dark.m-1");
+  }
+  
+  @Test
+  //DOWNLOAD TEMPLATE 3
+  public void downloadTemplate3() throws IOException, InterruptedException
+  {
+	  validlogin();
+	  DrbDownload  drbdwnld =new DrbDownload(driver);
+	  drbdwnld.clickTemplateBtn();
+	  Thread.sleep(500);
+	  drbdwnld.clickTemplate("#web-development > div > div > div:nth-child(1) > div:nth-child(2) > a > figure > figcaption");
+	  Thread.sleep(1000);
+	  drbdwnld.clickDownloadResume("body > app-root > app-twotemp > section > button.btn.btn.btn-dark.m-1");
+  }
+
+  
+    @Test(priority=4)
+    //EDIT PROFILE WITH VALID MANDATORY FIELDS
+    public void validEditProfile() throws IOException, InterruptedException
+  { 
   	  String name=ExcelUtilities.getcelldata(3, 1, 1);
   	  String dob=ExcelUtilities.getcelldata(3, 2, 1);
   	  String gender=ExcelUtilities.getcelldata(3, 3, 1);
@@ -211,8 +253,11 @@ public class DrbTest extends DrbBase{
    	  String lang=ExcelUtilities.getcelldata(3, 11, 1);
    	  String describe=ExcelUtilities.getcelldata(3, 12, 1);
    	  String img=System.getProperty("user.dir")+"\\"+ExcelUtilities.getcelldata(3, 13, 1);
+   	  
    	  dedtprf=new DrbEditProfile(driver);
+   	  
    	  validlogin();
+   	  
    	  dedtprf.clickMyProfile();
    	  dedtprf.clickEditProfilebtn();
    	  dedtprf.setName(name);
@@ -235,6 +280,7 @@ public class DrbTest extends DrbBase{
    
 
 @Test
+//CREATE PROFILE WITH VALID DATA - ALL FIELDS
 public void validAllCreateProfile() throws IOException, InterruptedException
 {
 	  
@@ -256,27 +302,18 @@ public void validAllCreateProfile() throws IOException, InterruptedException
  	  String gradPercentage=ExcelUtilities.getcelldata(2, 31, 1);
  	  String gradInstitute=ExcelUtilities.getcelldata(2, 32, 1);
  	  String gradYear=ExcelUtilities.getcelldata(2, 33, 1);
- 	  String postgrad=ExcelUtilities.getcelldata(2, 34, 1);
- 	  String postgradStream=ExcelUtilities.getcelldata(2, 35, 1);
- 	  String postgradPercentage=ExcelUtilities.getcelldata(2, 36, 1);
- 	  String postgradInstitute=ExcelUtilities.getcelldata(2, 37, 1);
- 	  String postgradYear=ExcelUtilities.getcelldata(2, 38, 1);
  	  String project=ExcelUtilities.getcelldata(2, 39, 1);
  	  String projectDesc=ExcelUtilities.getcelldata(2, 40, 1);
- 	  String additionalCourse=ExcelUtilities.getcelldata(2, 41, 1);
- 	  String courseName=ExcelUtilities.getcelldata(2, 42, 1);
- 	  String coursePercentage=ExcelUtilities.getcelldata(2, 43, 1);
- 	  String jobTitle=ExcelUtilities.getcelldata(2, 44, 1);
- 	  String from=ExcelUtilities.getcelldata(2, 45, 1);
- 	  String to=ExcelUtilities.getcelldata(2, 46, 1);
  	  String skills=ExcelUtilities.getcelldata(2, 47, 1);
  	  String lang=ExcelUtilities.getcelldata(2, 48, 1);
- 	  String programLang=ExcelUtilities.getcelldata(2, 49, 1);
  	  String  achievements=ExcelUtilities.getcelldata(2, 50, 1);
  	  String describe=ExcelUtilities.getcelldata(2, 51, 1);
  	  String img=System.getProperty("user.dir")+"\\"+ExcelUtilities.getcelldata(2, 52, 1);
+ 	  
  	  dcrtprf=new DrbCreateProfile(driver);
+ 	  
  	  validlogin();
+ 	  
  	  dcrtprf.clickMyProfile();
  	  dcrtprf.clickCrtProfile();
  	  dcrtprf.setName(name);
@@ -297,21 +334,9 @@ public void validAllCreateProfile() throws IOException, InterruptedException
 	  dcrtprf.setGraduationPercentage(gradPercentage);
 	  dcrtprf.setGraduationInstitute(gradInstitute);
 	  dcrtprf.setGraduationYear(gradYear);
-//	  dcrtprf.setPostGraduationStream(postgradStream);
-//	  dcrtprf.setPostGraduationPercentage(postgradPercentage);
-//	  dcrtprf.setPostGraduationInstitute(postgradInstitute);
-//	  dcrtprf.sePosttGraduationYear(postgradYear);
 	  dcrtprf.setProjectDetails(project,projectDesc);
-	  
-//	  dcrtprf.seAdditionalCourse(additionalCourse);
-//	  dcrtprf.setCourseName(courseName);
-//	  dcrtprf.setCoursePercentage(coursePercentage);
-//	  dcrtprf.setJobTitle(jobTitle);
-//	  dcrtprf.setFromDate(from);
-//	  dcrtprf.setToDate(to);
 	  dcrtprf.setSkills(skills);
  	  dcrtprf.setLanguage(lang);
-// 	  dcrtprf.setProgrammimgLanguages(programLang);
  	  dcrtprf.setAchievements(achievements);
  	  dcrtprf.setDescribe(describe);
  	  dcrtprf.setImage(img);
@@ -319,7 +344,9 @@ public void validAllCreateProfile() throws IOException, InterruptedException
  	  dcrtprf.clickCreateProfile();
  	 
 }
-//INVALID EDITPROFILE mandatory
+
+
+//INVALID EDITPROFILE - MANDATORY FIELDS
 @Test
 public void invalidEditProfile() throws IOException, InterruptedException
 {
@@ -352,28 +379,24 @@ public void invalidEditProfile() throws IOException, InterruptedException
  	  dedtprf.setPercent(percentage);
  	  dedtprf.setSchool(school);
  	  dedtprf.setYear(year);
- 	  
  	  dedtprf.setSkills(skills);
-
-
  	  dedtprf.setLanguage(lang);
  	  dedtprf.setDescribe(describe);
  	  dedtprf.setImage(img);
  	  dedtprf.acceptCheck();
-
- 	  dedtprf.clickUpdateProfile();	
+      dedtprf.clickUpdateProfile();	
 
 	  String expected ="Enter valid data";
 	  String actual =driver.switchTo().alert().getText();
-		Assert.assertEquals(actual,expected);
+	  Assert.assertEquals(actual,expected);
 		driver.switchTo().alert().accept();
  
 	  Thread.sleep(1000);         
 }
 
 
-
 @Test
+//EDIT PROFILE WITH ALL VALID FIELDS
 public void validAllEditProfile() throws IOException, InterruptedException
 {
 	  
@@ -395,22 +418,10 @@ public void validAllEditProfile() throws IOException, InterruptedException
 	  String gradPercentage=ExcelUtilities.getcelldata(3, 31, 1);
 	  String gradInstitute=ExcelUtilities.getcelldata(3, 32, 1);
 	  String gradYear=ExcelUtilities.getcelldata(3, 33, 1);
-	  String postgrad=ExcelUtilities.getcelldata(3, 34, 1);
-	  String postgradStream=ExcelUtilities.getcelldata(3, 35, 1);
-	  String postgradPercentage=ExcelUtilities.getcelldata(3, 36, 1);
-	  String postgradInstitute=ExcelUtilities.getcelldata(3, 37, 1);
-	  String postgradYear=ExcelUtilities.getcelldata(3, 38, 1);
 	  String project=ExcelUtilities.getcelldata(3, 39, 1);
 	  String projectDesc=ExcelUtilities.getcelldata(3, 40, 1);
-	  String additionalCourse=ExcelUtilities.getcelldata(3, 41, 1);
-	  String courseName=ExcelUtilities.getcelldata(3, 42, 1);
-	  String coursePercentage=ExcelUtilities.getcelldata(3, 43, 1);
-	  String jobTitle=ExcelUtilities.getcelldata(3, 44, 1);
-	  String from=ExcelUtilities.getcelldata(3, 45, 1);
-	  String to=ExcelUtilities.getcelldata(3, 46, 1);
 	  String skills=ExcelUtilities.getcelldata(3, 47, 1);
 	  String lang=ExcelUtilities.getcelldata(3, 48, 1);
-	  String programLang=ExcelUtilities.getcelldata(3, 49, 1);
 	  String  achievements=ExcelUtilities.getcelldata(3, 50, 1);
 	  String describe=ExcelUtilities.getcelldata(3, 51, 1);
 	  String img=System.getProperty("user.dir")+"\\"+ExcelUtilities.getcelldata(3, 52, 1);
@@ -438,21 +449,9 @@ public void validAllEditProfile() throws IOException, InterruptedException
 	  dedtprf.setGraduationPercentage(gradPercentage);
 	  dedtprf.setGraduationInstitute(gradInstitute);
 	  dedtprf.setGraduationYear(gradYear);
-//	  dedtprf.setPostGraduationStream(postgradStream);
-//	  dedtprf.setPostGraduationPercentage(postgradPercentage);
-//	  dedtprf.setPostGraduationInstitute(postgradInstitute);
-//	  dedtprf.sePosttGraduationYear(postgradYear);
-	  dedtprf.setProjectDetails(project,projectDesc);
-	  
-//	  dedtprf.seAdditionalCourse(additionalCourse);
-//	  dedtprf.setCourseName(courseName);
-//	  dedtprf.setCoursePercentage(coursePercentage);
-//	  dedtprf.setJobTitle(jobTitle);
-//	  dedtprf.setFromDate(from);
-//	  dedtprf.setToDate(to);
+	  dedtprf.setProjectDetails(project,projectDesc);	  
 	  dedtprf.setSkills(skills);
 	  dedtprf.setLanguage(lang);
-//	  dedttprf.setProgrammimgLanguages(programLang);
 	  dedtprf.setAchievements(achievements);
 	  dedtprf.setDescribe(describe);
 	  dedtprf.setImage(img);
@@ -461,31 +460,8 @@ public void validAllEditProfile() throws IOException, InterruptedException
 	
 	  Thread.sleep(1000);
 }
-@Test
-public  void invalidSignUp() throws IOException, InterruptedException
-{	  String username=ExcelUtilities.getcelldata(1, 0, 2);
-	  String phone=ExcelUtilities.getcelldata(1, 1, 2);
-	  String email=ExcelUtilities.getcelldata(1, 2, 2);
-	  String pass=ExcelUtilities.getcelldata(1, 3, 2);
-	  String cpass=ExcelUtilities.getcelldata(1, 4, 2);
-	  dsignup=new DrbSignUp(driver);
-	 // dsignup.clickSignUp();
-	 // dsignup.clickSignupLink();
-	  driver.navigate().to("http://167.71.226.96/signup");
-	  dsignup.setUserName(username);
-	  dsignup.setPhoneNumber(phone);
-	  dsignup.setMailId(ExcelUtilities.getcelldata(1, 2, 2));
-	  dsignup.setPassword(ExcelUtilities.getcelldata(1, 3, 2));
-	  dsignup.setConfirmPassword(ExcelUtilities.getcelldata(1, 4, 2));
-	  dsignup.clickRegister();
-	  
-	  WebElement registerButton=driver.findElement(By.cssSelector("body > app-root > app-signup > div > div > form > button > span"));
-		
-	  boolean actual=registerButton.isSelected();
-	  System.out.println(actual);
-	  Assert.assertFalse(actual);
-	  Thread.sleep(1000);  
-}
+
+
+
 
 }
-
